6c36bdbf71a4f0aa947cdd2389569f83cc6179cc
Merrill Lines <linesmerrill@gmail.com>
2023-04-21 09:43:47 -0700

    optimize code from google bard

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import Login from "./screens/Login";
-import Signup from "./screens/Signup";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
 
 const Stack = createStackNavigator();
 
-function App() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Signup} />
-    </Stack.Navigator>
-  );
-}
+const App = () => {
+  const [screen, setScreen] = React.useState("Login");
 
-export default () => {
   return (
     <NavigationContainer>
-      <App />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+export default App;
